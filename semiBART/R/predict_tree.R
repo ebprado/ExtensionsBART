25f9778ca7fa21e5f5214f46eea573c4bb0c3359
Estevão <estevao.prado.2019@mumail.ie>
2021-05-31 15:42:39 -0300

    Include formula

--- semiBART/R/predict_tree.R
+++ semiBART/R/predict_tree.R
@@ -42,70 +42,26 @@ predict_semibart = function(object, newdata_x1, newdata_x2,
   return(out)
 
 } # end of predict function
-#' @export
-marginal_predict_mybart = function(object, var_marg, newdata,
-                               type = c('all', 'median', 'mean')) {
-
-  # Create holder for predicted values
-  n_newX = dim(newdata)[1]
-  n_its = object$npost
-  ntrees = object$ntrees
-  y_hat_mat = matrix(0, nrow = n_its,
-                     ncol = nrow(newdata))
-
-  # Get which covariates are used by each tree in each MCMC iteration
-  vars_trees = matrix(NA, nrow=n_its, ncol=ntrees)
-  for (i in 1:n_its){
-    for (j in 1:ntrees){
-      aux = object$trees[[i]][[j]]$tree_matrix[,'split_variable']
-      if(length(aux) > 1){
-        vars_trees[i,j] = paste(unique(sort(aux[!is.na(aux)])), collapse = ',')
-      }
-    }
-  }
-
-  # Now loop through iterations and get predictions
-  for (i in 1:n_its) {
-    marginal_trees = which(vars_trees[i,] == var_marg)
-    # Sometimes the trees do not contain the variable we're interested in
-    if (length(marginal_trees) > 0){
-      # Get current set of trees
-      curr_trees = object$trees[[i]][marginal_trees]
-
-      # Use get_predictions function to get predictions
-      y_hat_mat[i,] = get_predictions(curr_trees,
-                                      newdata,
-                                      single_tree = length(curr_trees) == 1)
-
-    }
-  }
-
-  # Sort out what to return
-  out = list(vars_trees = vars_trees,
-             prediction = switch(type,
-                                 all = object$y_mean + object$y_sd * y_hat_mat,
-                                 mean = object$y_mean + object$y_sd * apply(y_hat_mat,2,'mean'),
-                                 median = object$y_mean + object$y_sd * apply(y_hat_mat,2,'median')))
-
-  return(out)
-
-} # end of predict function
-
 
 ########################################################################################################
 # Predictions for classification
 ########################################################################################################
 
 #' @export
-predict_mybart_class = function(object,
-                                   newdata,
-                                   type = c('all', 'median', 'mean')) {
+cl_predict_semibart = function(object, newdata_x1, newdata_x2,
+                            type = c('all', 'median', 'mean')) {
+
+
+  data_new = MakeDesignMatrixPredict(object$formula, newdata_x1)
+  X1 = as.matrix(data_new$X) # matrix to be used in the linear predictor
+  X2 = makeModelMatrixFromDataFrame(newdata_x2, drop = FALSE)
+  beta_hat = apply(object$beta_hat,2,mean)
 
   # Create holder for predicted values
-  n_newX = dim(newdata)[1]
+  n_newX = dim(X2)[1]
   n_its = object$npost
   y_hat_mat = matrix(NA, nrow = n_its,
-                     ncol = nrow(newdata))
+                     ncol = nrow(X2))
 
   # Now loop through iterations and get predictions
   for (i in 1:n_its) {
@@ -114,65 +70,15 @@ predict_mybart_class = function(object,
 
     # Use get_predictions function to get predictions
     y_hat_mat[i,] = get_predictions(curr_trees,
-                                    newdata,
+                                    X2,
                                     single_tree = length(curr_trees) == 1)
   }
 
-  # Sort out what to return
-  out = switch(type,
-               all = y_hat_mat,
-               mean = apply(pnorm(y_hat_mat),2,'mean'),
-               median = apply(pnorm(y_hat_mat),2,'median'))
-
-  return(out)
-
-} # end of predict function
-
-
-#' @export
-marginal_predict_mybart_class = function(object, var_marg, newdata,
-                                   type = c('all', 'median', 'mean')) {
-
-  # Create holder for predicted values
-  n_newX = dim(newdata)[1]
-  n_its = object$npost
-  ntrees = object$ntrees
-  y_hat_mat = matrix(0, nrow = n_its,
-                     ncol = nrow(newdata))
-
-  # Get which covariates are used by each tree in each MCMC iteration
-  vars_trees = matrix(NA, nrow=n_its, ncol=ntrees)
-  for (i in 1:n_its){
-    for (j in 1:ntrees){
-      aux = object$trees[[i]][[j]]$tree_matrix[,'split_variable']
-      if(length(aux) > 1){
-        vars_trees[i,j] = paste(unique(sort(aux[!is.na(aux)])), collapse = ',')
-      }
-    }
-  }
-
-  # Now loop through iterations and get predictions
-  for (i in 1:n_its) {
-    marginal_trees = which(vars_trees[i,] == var_marg)
-    # Sometimes the trees do not contain the variable we're interested in
-    if (length(marginal_trees) > 0){
-      # Get current set of trees
-      curr_trees = object$trees[[i]][marginal_trees]
-
-      # Use get_predictions function to get predictions
-      y_hat_mat[i,] = get_predictions(curr_trees,
-                                      newdata,
-                                      single_tree = length(curr_trees) == 1)
-
-    }
-  }
-
-  # Sort out what to return
-  out = list(vars_trees = vars_trees,
-             prediction = switch(type,
-                                 all    = pnorm(y_hat_mat),
-                                 mean   = apply(pnorm(y_hat_mat),2,'mean'),
-                                 median = apply(pnorm(y_hat_mat),2,'median')))
+    # Sort out what to return
+    out = switch(type,
+           all = X1%*%beta_hat + y_hat_mat,
+           mean = apply(pnorm(X1%*%beta_hat + y_hat_mat),2,'mean'),
+           median = apply(pnorm(X1%*%beta_hat + y_hat_mat),2,'median'))
 
   return(out)
 
